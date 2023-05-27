48d6cd021708ecae6dacf72823b73d6962856993
iuriseifriz <124320685+iuriseifriz@users.noreply.github.com>
2023-05-26 23:05:18 -0300

    Atualização 1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    
+// Modelo de inputs adicionais
     var input = '<label><input type="number" name="number" style="margin-left: 30px;" style="margin-bottom: 0px;"/> <a href="#" class="remove">X</a></label>';
-
+//Código de adição de inputs
     $("input[name='add']").click(function( e ){
       $('#inputs_adicionais').append( input );
     });
@@ -23,23 +23,6 @@ function contar(){
     }
     var media= soma/parseInt(allInputs.length);
 
-    /*if (Number.isNaN(media) return false){
-        res.innerHTML = `erro`
-        else{
-            res.innerHTML = `${media}`
-        }
-
-    }
-}
-*/
-
     res.innerHTML = `${media}`
 
 }
-
-
-
-    /*if (n1.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados')
-} 
-*/
